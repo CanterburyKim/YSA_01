50f3da417c1bb6b84ffe3cf2bb128a8c4122461f
Goro <evilninjax@gmail.com>
2019-06-19 19:20:39 -0400

    commenting out all code.  We will uncomment lines as needed

--- 01_print.py
+++ 01_print.py
@@ -1,35 +1,35 @@
-# Part 1
-print('Hello World')
-
-# Part 2
-my_message = 'Hello World'
-print(my_message)
-
-# Part 3
-my_second_message = 'Hi mom!'
-print('my_second_message')
-
-# Part 4 - "adding" (appending) strings
-another_message = 'Hello'
-yet_another_message = 'Boo Boo'
-
-print(another_message + yet_another_message)
-print(another_message + ' ' + yet_another_message)
-
-# Part 4b
-print(yet_another_message + ' ' + another_message)
-
-# Part 5 - formatted strings (f-strings)
-print(f'{another_message} {yet_another_message}')
-
-# Part 6 - nums
-num = 12
-some_message = 'Goodbye and Goodnight, '
-print( f'{some_message} {num}')
-
-# Part 7 - appending nums
-some_num = '1'
-other_num = '1'
-result = 1 + 1
-print(some_num + other_num)
-print(f'{some_num}+{other_num}={result}')
+# # Part 1
+# print('Hello World')
+#
+# # Part 2
+# my_message = 'Hello World'
+# print(my_message)
+#
+# # Part 3
+# my_second_message = 'Hi mom!'
+# print('my_second_message')
+#
+# # Part 4 - "adding" (appending) strings
+# another_message = 'Hello'
+# yet_another_message = 'Boo Boo'
+#
+# print(another_message + yet_another_message)
+# print(another_message + ' ' + yet_another_message)
+#
+# # Part 4b
+# print(yet_another_message + ' ' + another_message)
+#
+# # Part 5 - formatted strings (f-strings)
+# print(f'{another_message} {yet_another_message}')
+#
+# # Part 6 - nums
+# num = 12
+# some_message = 'Goodbye and Goodnight, '
+# print( f'{some_message} {num}')
+#
+# # Part 7 - appending nums
+# some_num = '1'
+# other_num = '1'
+# result = 1 + 1
+# print(some_num + other_num)
+# print(f'{some_num}+{other_num}={result}')
